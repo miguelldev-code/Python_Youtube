2d0628e225096b31406257c70f65121e3252a6cf
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-02-26 00:22:22 -0500

    Creacion ejercicio 3

---
+++ E3_gestorContactos.py
@@ -0,0 +1,11 @@
+"""
+📌 Gestor de Contactos en Python  
+Este programa permite agregar, buscar, eliminar y mostrar contactos  
+usando un diccionario en Python. 
+ 
+Conceptos Técnicos Aprendidos
+🔹 Uso de diccionarios para almacenar contactos.
+🔹 Funciones para organizar el código.
+🔹 Bucle while con un menú interactivo.
+🔹 Entrada y salida de datos con input() y print().
+"""
